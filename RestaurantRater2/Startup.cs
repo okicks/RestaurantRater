6256aa4ab1e82e30e6cd435d017c30d75b644f20
Oliver Kicks <kicks.oliver@gmail.com>
2019-08-21 20:38:11 -0400

    Add project files.

---
+++ RestaurantRater2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RestaurantRater2.Startup))]
+namespace RestaurantRater2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
